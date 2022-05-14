45eb14c9983c6a5cc1a0e88dc4f126f4b270ac25
Bel-Pet <80157339+Bel-Pet@users.noreply.github.com>
2022-05-14 20:14:55 +0700

    fix

--- bencodeParser/src/main/java/Main.java
+++ bencodeParser/src/main/java/Main.java
@@ -1,24 +1,37 @@
 import error.*;
 import lexer.Lexer;
 import lexer.Token;
+import parser.Expr;
+import parser.Parser;
 
-import java.io.BufferedReader;
-import java.io.StringReader;
+import java.io.*;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        String text = "d i123e 3:sdf i13e" +
-                "i45ee 4:5493s";
 
-        BufferedReader br = new BufferedReader(new StringReader(text));
-        ConsoleReporter reporter = new ConsoleReporter();
-
-        String result = Interpreter.interpret(br, reporter);
-        if (result == null) {
-            System.out.println(reporter);
-            return;
+    public static String interpret(BufferedReader br, Reporter reporter) {
+        List<Token> tokens = Lexer.scan(br, reporter);
+        if (tokens == null) {
+            for (String v: reporter.getReporters()) {
+                System.out.println(v);
+            }
+            return null;
         }
-        System.out.println(result);
+        List<Expr> expressions = Parser.parse(tokens, reporter);
+        if (expressions != null) {
+            return Interpreter.interpret(expressions);
+        }
+        for (String v: reporter.getReporters()) {
+            System.out.println(v);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new FileReader("src/main/resources/bencode.torrent"));
+        Reporter reporter = new Reporter(5);
+        String str = interpret(input, reporter);
+        if (str != null)
+            System.out.println(str);
     }
 }
