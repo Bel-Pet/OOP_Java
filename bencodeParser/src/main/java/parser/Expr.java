347698c17f0af15f50cd79f2bbf8e069d9a01df3
Bel-Pet <80157339+Bel-Pet@users.noreply.github.com>
2022-05-14 20:16:00 +0700

    fix

--- bencodeParser/src/main/java/parser/Expr.java
+++ bencodeParser/src/main/java/parser/Expr.java
@@ -3,16 +3,13 @@ package parser;
 import java.util.List;
 import java.util.Map;
 
-
 public sealed interface Expr {
 
-    record Line(String line) implements Expr {}
+    record Line(String value) implements Expr {}
 
-    // CR: int number
-    record Number(String number) implements Expr {}
+    record Number(Integer value) implements Expr {}
 
-    record Array(List<Expr> list) implements Expr {}
+    record Array(List<Expr> value) implements Expr {}
 
-    // CR: key should be Line, not Expr
-    record Dictionary(Map<Expr, Expr> dictionary) implements Expr {}
+    record Dictionary(Map<String, Expr> value) implements Expr {}
 }
