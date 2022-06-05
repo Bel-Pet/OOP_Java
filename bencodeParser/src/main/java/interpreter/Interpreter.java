2be3d636e4a637c758a39c96693445ff2d76f028
Bel-Pet <80157339+Bel-Pet@users.noreply.github.com>
2022-06-06 04:42:51 +0700

    add

---
+++ bencodeParser/src/main/java/interpreter/Interpreter.java
@@ -0,0 +1,72 @@
+package interpreter;
+
+import parser.Expr;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Interpreter {
+    List<Expr> expressions;
+    int numberDictionaries;
+
+    private Interpreter(List<Expr> expressions) {
+        this.expressions = expressions;
+    }
+
+    public static String interpret(List<Expr> expressions) {
+        Interpreter interpreter = new Interpreter(expressions);
+        return interpreter.interpret();
+    }
+
+    private String interpret() {
+        return expressions.stream().map(this::checkExpression).collect(Collectors.joining("\n"));
+    }
+
+    private String checkExpression(Expr expr) {
+        return switch (expr) {
+            case Expr.Line n -> getLine(n);
+            case Expr.Number n -> getNumber(n);
+            case Expr.Array n -> getArray(n);
+            case Expr.Dictionary n -> getDictionary(n);
+        };
+    }
+
+    private String getLine(Expr.Line line) {
+        return '"' + line.value() + '"';
+    }
+
+    private String getNumber(Expr.Number line) {
+        return line.value().toString();
+    }
+
+    private String getArray(Expr.Array line) {
+        return  "["
+                + line.value().stream().map(this::checkExpression).collect(Collectors.joining(", "))
+                + "]";
+    }
+
+    private String getDictionary(Expr.Dictionary dictionary) {
+        StringJoiner str = new StringJoiner("\n");
+
+        if (numberDictionaries > 0) str.add("");
+        str.add(" ".repeat(numberDictionaries) + "{");
+        numberDictionaries++;
+
+        List<String> list = new LinkedList<>(dictionary.value().keySet());
+        Collections.sort(list);
+        str.add(list.stream().map(key -> addMapEntry(dictionary.value(), key)).collect(Collectors.joining(",\n")));
+        /*str.append(dictionary.value().keySet()
+                                        .stream().map(key -> addMapEntry(dictionary.value(), key))
+                                        .collect(Collectors.joining(",\n")));*/
+        numberDictionaries--;
+        str.add(" ".repeat(numberDictionaries) + "}");
+
+        return str.toString();
+    }
+
+    private String addMapEntry(LinkedHashMap<String, Expr> map, String key) {
+        return " ".repeat(numberDictionaries)
+                + "\"" + key + "\""
+                + ": " + checkExpression(map.get(key));
+    }
+}
