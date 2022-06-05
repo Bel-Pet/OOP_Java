e74d9b379ae224a932eff24b74d5ca49749830af
Bel-Pet <80157339+Bel-Pet@users.noreply.github.com>
2022-06-06 04:43:49 +0700

    add

--- bencodeParser/src/main/java/test/java/lexer/LexerTest.java
+++ bencodeParser/src/main/java/test/java/lexer/LexerTest.java
@@ -42,22 +42,25 @@ public class LexerTest {
     }
 
     @Test
+    public void unknownCharInNumber() {
+        assertNull(getTokenTypes("1f1:wertyuiopas"));
+        assertNull(getTokenTypes("i4g5e"));
+    }
+
+    @Test
+    public void emptyNumber() {
+        assertNull(getTokenTypes("ie"));
+    }
+
+    @Test
     public void noColonInString() {
         assertNull(getTokenTypes("45bgf"));
+        assertNull(getTokenTypes("45"));
     }
 
     @Test
     public void missingCharsInString() {
         assertNull(getTokenTypes("5:we"));
-    }
-
-    @Test
-    public void unknownCharInNumber() {
-        assertNull(getTokenTypes("1f1:wertyuiopas i4g5e"));
-    }
-
-    @Test
-    public void expectedNumberInType() {
-        assertNull(getTokenTypes("ie"));
+        assertNull(getTokenTypes("45:"));
     }
 }
