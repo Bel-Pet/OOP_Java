d2d99038dc0b6f57105e9b69a3bbca1e20c0d18e
Bel-Pet <80157339+Bel-Pet@users.noreply.github.com>
2022-06-05 14:19:38 +0700

    add

--- bencodeParser/src/main/java/test/java/InterpreterTest.java
+++ bencodeParser/src/main/java/test/java/InterpreterTest.java
@@ -10,96 +10,131 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-/*
-1. string
-2. integer
-3. dict empty
-4. dict one element - primitive
-5. list empty
-6. list one element - primitive
-7. dict in dict
-8. list in dict
-9. list in list
-10. dict in list
- */
-
 class InterpreterTest {
 
-    private String getJsonText(String text, int maxMessages) {
+    private String getJasonText(String text) {
         BufferedReader br = new BufferedReader(new StringReader(text));
-        Reporter reporter = new Reporter(maxMessages);
+        Reporter reporter = new Reporter(10);
+
         List<Expr> expressions = Parser.parse(Lexer.scan(br, reporter), reporter);
+
         return expressions == null ? null : Interpreter.interpret(expressions);
     }
 
     @Test
-    public void expressionsIsNull() {
-        assertNull(getJsonText("", 5));
+    public void empty() {
+        assertNull(getJasonText(""));
     }
 
     @Test
-    public void noAsciiCharInString() {
-        String res = getJsonText("4:e\u0660\u06F0i", 4);
-        assertNotNull(res);
-        assertEquals("\"ei\"", res);
+    public void oneString() {
+        assertEquals("\"qwerty\"", getJasonText("6:qwerty"));
     }
 
     @Test
-    public void minorErrors() {
-        String input = """
-                i4x5e
-                d t
-                4:er\u06F0t i34e
-                2:rr l i45e i66e e
-                2:gg d
-                3:vfr i76es
-                e
-                e
-                i55e
-                k
-                """;
+    public void oneNumber() {
+        assertEquals("456", getJasonText("i456e"));
+    }
+
+    @Test
+    public void emptyDictionary() {
         String output = """
-                45
                 {
-                 rr: [66],
-                 gg:\s
-                 {
-                  vfr: 76
-                 },
-                 ert: 34
-                }
-                55""";
-        String res = getJsonText(input, 7);
-        assertNotNull(res);
-        assertEquals(output, res);
+                
+                }""";
+        assertEquals(output, getJasonText("de"));
     }
 
     @Test
-    public void correctData() {
+    public void oneElementDictionary() {
+        String output = """
+                {
+                 adas: 6
+                }""";
+        assertEquals(output, getJasonText("d 4:adas i6e e"));
+    }
+
+    @Test
+    public void dictionaryInDictionary() {
+        String output = """
+                {
+                 qwe:\s
+                 {
+                  adas: 6
+                 }
+                }""";
+        assertEquals(output, getJasonText("d 3:qwe d 4:adas i6e e e"));
+    }
+
+    @Test
+    public void listInDictionary() {
+        String output = """
+                {
+                 qwe: [456]
+                }""";
+        assertEquals(output, getJasonText("d 3:qwe l i456e e e"));
+    }
+
+    @Test
+    public void emptyList() {
+        assertEquals("[]", getJasonText("le"));
+    }
+
+    @Test
+    public void oneElementList() {
+        assertEquals("[543]", getJasonText("l i543e e"));
+    }
+
+    @Test
+    public void listInList() {
+        assertEquals("[[543]]", getJasonText("l l i543e e e"));
+    }
+
+    @Test
+    public void dictionaryInList() {
+        String output = """
+                [{
+                 rty: 456
+                }]""";
+        assertEquals(output, getJasonText("l d 3:rty i456e e e"));
+    }
+
+    @Test
+    public void complexData() {
         String input = """
-                i45e
                 d
-                3:ert i34e
-                2:rr l i45e i66e e
-                2:gg d
-                3:vfr i76e
+                3:bsd i5e
+                4:adas i6e
+                3:vfd l 4:dfgg 3:bgf i45e e
+                3:fgh l l i435e 5:qwert
+                    d   2:gt i12e
+                        4:vfrd 2:rt
+                    e
+                    i78e
+                    e
+                    e
+                6:kjftgy d
+                         3:kds i345e
+                         e
                 e
-                e
-                i55e
                 """;
+
         String output = """
-                45
                 {
-                 rr: [66],
-                 gg:\s
+                 adas: 6,
+                 bsd: 5,
+                 fgh: [[435, "qwert",\s
                  {
-                  vfr: 76
+                  gt: 12,
+                  vfrd: "rt"
+                 }, 78]],
+                 kjftgy:\s
+                 {
+                  kds: 345
                  },
-                 ert: 34
-                }
-                55""";
-        String res = getJsonText(input, 7);
-        assertNotNull(res);
-        assertEquals(output, res);
+                 vfd: ["dfgg", "bgf", 45]
+                }""";
+
+        assertEquals(output, getJasonText(input));
     }
 }
