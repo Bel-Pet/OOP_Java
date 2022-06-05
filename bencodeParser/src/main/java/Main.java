770dab646a46747cd9460a884d3de5932ec669e2
Bel-Pet <80157339+Bel-Pet@users.noreply.github.com>
2022-06-05 14:21:51 +0700

    add

--- bencodeParser/src/main/java/Main.java
+++ bencodeParser/src/main/java/Main.java
@@ -9,25 +9,30 @@ import java.util.List;
 
 public class Main {
 
-    public static String interpret(BufferedReader br, Reporter reporter) {
+    public static String interpret(BufferedReader br, int limitErrorMessages) {
+        Reporter reporter = new Reporter(limitErrorMessages);
         List<Token> tokens = Lexer.scan(br, reporter);
+
         if (tokens == null) {
-            reporter.getReporters().forEach(System.out::println);
+            System.out.println("Errors found: " + reporter.getNumberErrors()
+                                + "\nLimit error messages: " + limitErrorMessages);
             return null;
         }
+
         List<Expr> expressions = Parser.parse(tokens, reporter);
-        if (expressions != null) {
-            return Interpreter.interpret(expressions);
-        }
-        reporter.getReporters().forEach(System.out::println);
+        if (expressions != null) return Interpreter.interpret(expressions);
+
         return null;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new FileReader("src/main/resources/bencode.torrent"));
-        Reporter reporter = new Reporter(5);
-        String str = interpret(input, reporter);
-        if (str != null)
-            System.out.println(str);
+
+        Writer out = new FileWriter("src/main/resources/out.json");
+
+        String str = interpret(input, 10);
+        if (str != null) out.write(str);//System.out.println(str);
+
+        out.close();
     }
 }
