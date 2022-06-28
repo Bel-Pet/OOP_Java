eea9fd809dd8885c5fdd93942fb9d96503d6f161
Bel-Pet <80157339+Bel-Pet@users.noreply.github.com>
2022-06-28 10:56:46 +0700

    fix

--- bencodeParser/src/main/java/parser/Parser.java
+++ bencodeParser/src/main/java/parser/Parser.java
@@ -38,26 +38,33 @@ public class Parser {
     }
 
     private Expr parseExpr() throws TranslateBencodeException {
-        // CR: why move position forward and then do -1 everywhere?
-        position++;
-
-        return switch (tokens.get(position - 1).tokenType()) {
+        return switch (tokens.get(position).tokenType()) {
             case LIST -> parseList();
             case DICTIONARY -> parseDictionary();
-            case STRING -> new Expr.Line((String) tokens.get(position - 1).value());
-            case INTEGER -> new Expr.Number((Integer) tokens.get(position - 1).value());
+            case STRING -> parseString();
+            case INTEGER -> parseInteger();
             default -> {
                 String message = unexpectedToken("Expected value",
-                        tokens.get(position - 1),
-                        TokenType.INTEGER, TokenType.STRING, TokenType.LIST, TokenType.DICTIONARY);
+                        tokens.get(position), TokenType.INTEGER, TokenType.STRING, TokenType.LIST, TokenType.DICTIONARY);
 
                 throw new TranslateBencodeException(message);
             }
         };
     }
 
+    private Expr parseString() {
+        position++;
+        return new Expr.Line((String) tokens.get(position - 1).value());
+    }
+
+    private Expr parseInteger() {
+        position++;
+        return new Expr.Number((Integer) tokens.get(position - 1).value());
+    }
+
     private Expr parseList() throws TranslateBencodeException {
-        int startType = position - 1;
+        int startType = position;
+        position++;
 
         List<Expr> list = new ArrayList<>();
 
@@ -71,14 +78,15 @@ public class Parser {
         }
 
         throw new TranslateBencodeException(unexpectedToken("No end complex char, complex type:",
-                                                            tokens.get(startType),
-                                                            TokenType.TYPE_END));
+                                                            tokens.get(startType), TokenType.TYPE_END));
     }
 
     private Expr parseDictionary() throws TranslateBencodeException {
-        int startType = position - 1;
+        int startType = position;
+        position++;
 
         LinkedHashMap<String, Expr> map = new LinkedHashMap<>();
+        String previousKey = null;
 
         while (position < tokens.size()) {
             if (tokens.get(position).tokenType() == TokenType.TYPE_END) {
@@ -86,48 +94,30 @@ public class Parser {
                 return new Expr.Dictionary(map);
             }
 
-            String key = addKey(map);
+            String key = addKey(previousKey);
+            previousKey = key;
 
-            if (position >= tokens.size()) {
-                String message = "Expected value and end complex type in end of file";
-                throw new TranslateBencodeException(message);
-            }
+            if (position >= tokens.size())
+                throw new TranslateBencodeException("Expected value and end complex type in end of file");
 
             Expr value = parseExpr();
             map.put(key, value);
         }
 
-        throw new TranslateBencodeException(unexpectedToken("No end complex char, complex type:",
-                                                            tokens.get(startType),
-                                                            TokenType.TYPE_END));
+        throw new TranslateBencodeException(unexpectedToken("No end complex char, complex type:"
+                                                            , tokens.get(startType), TokenType.TYPE_END));
     }
 
-    private String addKey(LinkedHashMap<String, Expr> map) throws TranslateBencodeException {
-        if (tokens.get(position).tokenType() != TokenType.STRING) {
-            String message = unexpectedToken("Invalid key", tokens.get(position), TokenType.STRING);
-            throw new TranslateBencodeException(message);
-        }
+    private String addKey(String previousKey) throws TranslateBencodeException {
+        if (tokens.get(position).tokenType() != TokenType.STRING)
+            throw new TranslateBencodeException(unexpectedToken("Invalid key"
+                                                , tokens.get(position), TokenType.STRING));
 
         String key = (String) tokens.get(position).value();
 
-        if (map.containsKey(key)) {
-            String message = unexpectedToken("Repeating key", tokens.get(position), TokenType.STRING);
-            throw new TranslateBencodeException(message);
-        }
-
-        if (map.size() == 0) {
-            position++;
-            return key;
-        }
-
-        // CR: please think about why is this wrong, fix it and tell me the answer
-        String lastKeyMap = (String) map.keySet().toArray()[map.size() - 1];
-
-        // CR: we can just keep last inserted key, or null if we haven't inserted any yet
-        if (lastKeyMap.compareTo(key) >= 0) {
-            String message = unexpectedToken("Wrong key order", tokens.get(position), TokenType.STRING);
-            throw new TranslateBencodeException(message);
-        }
+        if (previousKey != null && previousKey.compareTo(key) >= 0)
+            throw new TranslateBencodeException(unexpectedToken("Wrong key order"
+                                                                , tokens.get(position), TokenType.STRING));
 
         position++;
         return key;
